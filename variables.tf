ad2c497635769b9dfa4e506859bb72484bdc63af
Dev-PEB <118755228+Dev-PEB@users.noreply.github.com>
2024-07-19 21:19:22 +0100

    basic tf codes for ec2 and vpcsetup
    for testing before pipline is completed

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+}
+
+variable "instance_type" {
+  description = "Type of the instance"
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
+}

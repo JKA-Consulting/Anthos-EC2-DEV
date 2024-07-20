7658131689c83dc4c144eb0ab4b925209c46d43e
Abasi-ifreke Uwem <uwemabasifrekelive@gmail.com>
2024-07-20 08:56:19 +0100

    added pipeline yaml file

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "my_instance" {
-  ami           = "ami-f976839e" 
+  ami           = "ami-f976839e"
   instance_type = var.instance_type
   key_name      = var.key_name
   subnet_id     = aws_subnet.main.id
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,3 @@
-aws_region = "eu-west-1"
+aws_region    = "eu-west-1"
 instance_type = "t2.micro"
-key_name = "your-key-pair-name"
+key_name      = "your-key-pair-name"

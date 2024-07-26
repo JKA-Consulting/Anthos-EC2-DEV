a1c317b9134e2ba0dae6800e53c6e7a424ae5af4
Dev-PEB <118755228+Dev-PEB@users.noreply.github.com>
2024-07-26 21:16:55 +0100

    tf codes
    working terraform codes intended for infra setup with minimal manual intervention

---
+++ network.tf
@@ -0,0 +1,50 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "${var.aws_region}a"
+  tags = {
+    Name = "main-subnet"
+  }
+}
+resource "aws_security_group" "main_sg" {
+  name        = "main_sg"
+  description = "Security group for my instance"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allows SSH access from anywhere (use with caution)
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allows HTTP traffic
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allows HTTPS traffic
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # Allows all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+}
+
+variable "instance_type" {
+  description = "Type of the instance"
+}
